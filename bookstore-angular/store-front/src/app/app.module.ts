4b27fd42db97980e9830ea5676d37a537f0bc44f
Tiedros Abrha <tiedrosabrha@gmail.com>
2019-12-22 10:17:30 -0600

    Initial set up for store front

--- bookstore-angular/store-front/src/app/app.module.ts
+++ bookstore-angular/store-front/src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Imports
+import { MatInputModule,
+ 		 MatButtonModule,
+ 		 MatMenuModule,
+ 		 MatToolbarModule, 
+ 		 MatSelectModule, 
+ 		 MatIconModule } from '@angular/material';
+import 'hammerjs';
+// Component Import
+
+// service Import
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+	     BrowserModule,
+	     AppRoutingModule,
+	     MatInputModule,
+		 MatButtonModule,
+		 MatMenuModule,
+		 MatToolbarModule, 
+		 MatSelectModule, 
+		 MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
